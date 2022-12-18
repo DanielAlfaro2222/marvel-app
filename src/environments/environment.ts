9f815f6a571800c1b8dc38bd35211102c8f2e7e6
Daniel Alfaro <69684433+DanielAlfaro2222@users.noreply.github.com>
2022-12-18 18:41:58 -0500

    Delete environment.ts

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-
-export const environment = {
-   production: false,
-   URL_API: 'https://gateway.marvel.com:443/',
-   API_KEY: "c493a0bb5452e72b00cb160d7e6b6ab8",
-   HASH: "6b12b46522c71549db8dc958dabc5aaf"
-};
